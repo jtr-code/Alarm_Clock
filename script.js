4438b4dc98f123738193052370e9dce7d9716b61
jtr-code <jtrtricks@gmail.com>
2022-11-10 23:57:35 +0530

    new model

--- script.js
+++ script.js
@@ -1,41 +1,89 @@
-function currentTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
-    let session = "AM";
 
-//12-Hour format
-    
-    if (hh == 0) {
-        hh = 12;
+
+var alarmString = null;
+
+// Select HTML5 Audio element
+let alarmAudio = new Audio('audio.mp3')
+
+// Select DOM element with create-alarm id
+const createAlarm = document.querySelector(".create-alarm");
+
+// Select DOM element of active alarm container
+const activeAlarm = document.getElementById("active-alarm");
+const clearAlarm = document.getElementById("clear-alarm");
+
+// Select DOM element of active alarm text
+const alarmTextContainer = document.getElementById("alarm-text");
+
+const alarmText = (time) => `Alarm set at time ${time}`;
+
+
+// Handle Create Alarm submit
+const handleSubmit = (event) => {
+    // Prevent default action of reloading the page
+    event.preventDefault();
+    const { hour, sec, min, zone } = document.forms[0];
+    alarmString = getTimeString({
+        hours: hour.value,
+        seconds: sec.value,
+        minutes: min.value,
+        zone: zone.value
+    });
+    // Reset form after submit
+    document.forms[0].reset();
+    // Hide create alarm
+    createAlarm.style.display = "none";
+    // show active alarm with text
+    activeAlarm.style.display = "block";
+    alarmTextContainer.innerHTML = alarmText(alarmString);
+};
+
+const handleClear = () => {
+
+    alarmAudio.pause();
+    alarmString = "";
+    activeAlarm.style.display = "none";
+    createAlarm.style.display = "block";
+};
+
+
+clearAlarm.addEventListener("click", handleClear);
+// Attach submit event to the form
+document.forms[0].addEventListener("submit", handleSubmit);
+
+// Function to check if alarm needs to be triggered
+const checkAlarm = (timeString) => {
+    if (alarmString === timeString) {
+        alarmAudio.play();
     }
-    if (hh > 12) {
-        hh = hh - 12;
-        session = "PM";
+};
+
+// Function to convert time to string value
+const getTimeString = ({ hours, minutes, seconds, zone }) => {
+    if (minutes / 10 < 1) {
+        minutes = "0" + minutes;
     }
+    if (seconds / 10 < 1) {
+        seconds = "0" + seconds;
+    }
+    return `${hours}:${minutes}:${seconds} ${zone}`;
+};
 
-    hh = (hh < 10) ? "0" + hh : hh;
-    mm = (mm < 10) ? "0" + mm : mm;
-    ss = (ss < 10) ? "0" + ss : ss;
+// Function to display current time on screen
+const renderTime = () => {
+    var currentTime = document.getElementById("current-time");
+    const currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    var zone = hours >= 12 ? "PM" : "AM";
+    if (hours > 12) {
+        hours = hours % 12;
+    }
+    const timeString = getTimeString({ hours, minutes, seconds, zone });
+    checkAlarm(timeString);
+    currentTime.innerHTML = timeString;
+};
 
-    document.getElementById('hour').innerHTML = hh;
-    document.getElementById('minute').innerHTML = mm;
-    document.getElementById('second').innerHTML = ss;
-    document.getElementById('session').innerHTML = session;
-
-}
-setInterval(currentTime, 1000);
-
-
-function audioPlay() {
-    setTimeout(() => {
-        let audio = new Audio('audio.mp3');
-        audio.loop = true;
-        audio.play();
-
-    }, 1000);
-
-
-}
-// audioPlay();
+// Update time every second
+setInterval(renderTime, 1000);
